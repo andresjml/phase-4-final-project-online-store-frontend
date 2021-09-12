2a7dd4e61f8264d8887714efe8f22e29254edbd9
Andres <andresjml@gmail.com>
2021-09-12 10:54:47 -0400

    adding react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import NavBar from "./components/NavBar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 
 function App() {
   return (
-    <div>React App</div>
+    <Router>
+      <div>React App</div>
+      <NavBar />
+      <Switch>
+        <Route exact path='/'><Home/></Route>
+      </Switch>
+    </Router>
   );
 }
 
