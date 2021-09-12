e65e5ca251a43c0c7176300e95ffbd891858a5db
Andres <andresjml@gmail.com>
2021-09-12 12:44:16 -0400

    moving Route out of App component to Index.js component, setting up login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
+import React,{useState, useEffect} from 'react'
+import {BASE_URL} from '../src/constraints/index'
 import NavBar from "./components/NavBar";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 import Home from '../src/components/Home'
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route path='/login'>
-          <Login/>
-        </Route>
-        <Route path= '/signup'>
-          <Signup/>
-        </Route>
+  const [user, setUser] = useState(null);
 
-      </Switch>
-    </Router>
-  );
+  useEffect(() => {
+    fetch(BASE_URL+"/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (user) {
+    return (
+      <>
+      <h2>Welcome, {user.username}!</h2>
+      
+        <NavBar />
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+          <Route path= '/signup'>
+            <Signup/>
+          </Route>
+  
+        </Switch>
+      
+      </>
+    );
+  } else {
+    return <Login onLogin={setUser} />;
+  }
 }
+
+  
+
 
 export default App;
