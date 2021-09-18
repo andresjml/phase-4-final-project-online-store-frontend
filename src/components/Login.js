c167dad73e135218669269b2cd64ed8e2b99b76f
Andres <andresjml@gmail.com>
2021-09-18 11:16:03 -0400

    handling errors Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import {BASE_URL} from '../constraints/index'
 
 
 function Login({onLogin}) {
-    const [username, setUsername] = useState("");
+    const [username, setUsername] = useState("")
     const [toggle, setToggle]=useState(false)
     const [newUser, setNewUser]=useState({   
         username:"",
@@ -12,6 +12,11 @@ function Login({onLogin}) {
         first_name:"",
         last_name:""
     })
+    const [errors, setErrors] = useState(null)
+    const [errorSignUp, setErrorSignUp]=useState(null)
+
+
+
 
     function handleSignupClick(){
         setToggle(!toggle)
@@ -24,12 +29,18 @@ function Login({onLogin}) {
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username: username }),
+        body: JSON.stringify({ username }),
         })
-        .then((r) => r.json())
-        .then((user) => onLogin(user));
+        .then((response) => {
+            if (response.ok) {
+            response.json().then((resp) => onLogin(resp));
+          }else {
+            response.json().then((errorData) => setErrors(errorData));
+          }
+        })
     }
 
+    console.log(errors)
     //UPDATED FORM
     function handleInputChange(event) {
         setNewUser({
@@ -49,8 +60,13 @@ function Login({onLogin}) {
         },
         body: JSON.stringify(newUser),
         })
-        .then((r) => r.json())
-        .then((user) => onLogin(user));
+        .then((response) => {
+            if (response.ok) {
+            response.json().then((resp) => onLogin(resp));
+          }else {
+            response.json().then((errorData) => setErrorSignUp(errorData.errors));
+          }
+        })
     }
 
 
@@ -66,34 +82,56 @@ function Login({onLogin}) {
                         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
                         <button className='btn btn-dark' type="submit">Login</button>
                     </form>
+                    {
+                        errors && (
+
+                            <div class="alert alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>Wait!</strong> The username is incorrect
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=>setErrors(null)}></button>
+                            </div>
+                            
+                        )
+                    }
                 </div>
                 <div className="col">
                     <h3>New user?</h3>
                     <button type="submit" className='btn btn-dark' onClick={handleSignupClick}>Signup</button>  
                     {toggle&&(
-                        <form onSubmit={handleNewUserSubmit}>
-                            <div className="input-group mb-3">
-                                <span className="input-group-text" id="basic-addon1">Username</span>
-                                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="username" value={newUser.username} onChange={handleInputChange}/>
-                            </div>
-                            <div className="input-group mb-3">
-                                <span className="input-group-text" id="basic-addon1">Address</span>
-                                <input type="text" className="form-control" placeholder="Address" aria-label="Username" aria-describedby="basic-addon1" name="address" value={newUser.address}onChange={handleInputChange}/>
-                            </div>
-                            <div className="input-group mb-3">
-                                <span className="input-group-text" id="basic-addon1">Phone</span>
-                                <input type="text" className="form-control" placeholder="Phone" aria-label="Username" aria-describedby="basic-addon1" name="phone" value={newUser.phone}onChange={handleInputChange}/>
-                            </div>
-                            <div className="input-group mb-3">
-                                <span className="input-group-text" id="basic-addon1">First Name</span>
-                                <input type="text" className="form-control" placeholder="First Name" aria-label="Username" aria-describedby="basic-addon1" name="first_name" value={newUser.first_name} onChange={handleInputChange}/>
-                            </div>
-                            <div className="input-group mb-3">
-                                <span className="input-group-text" id="basic-addon1">Last Name</span>
-                                <input type="text" className="form-control" placeholder="Last Name" aria-label="Username" aria-describedby="basic-addon1" name="last_name" value={newUser.last_name} onChange={handleInputChange}/>
-                            </div>
-                            <button className='btn btn-dark' type="submit">Login</button>
-                        </form>
+                        <>
+                            <form onSubmit={handleNewUserSubmit}>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="basic-addon1">Username</span>
+                                    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="username" value={newUser.username} onChange={handleInputChange}/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="basic-addon1">Address</span>
+                                    <input type="text" className="form-control" placeholder="Address" aria-label="Username" aria-describedby="basic-addon1" name="address" value={newUser.address}onChange={handleInputChange}/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="basic-addon1">Phone</span>
+                                    <input type="text" className="form-control" placeholder="Phone" aria-label="Username" aria-describedby="basic-addon1" name="phone" value={newUser.phone}onChange={handleInputChange}/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="basic-addon1">First Name</span>
+                                    <input type="text" className="form-control" placeholder="First Name" aria-label="Username" aria-describedby="basic-addon1" name="first_name" value={newUser.first_name} onChange={handleInputChange}/>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text" id="basic-addon1">Last Name</span>
+                                    <input type="text" className="form-control" placeholder="Last Name" aria-label="Username" aria-describedby="basic-addon1" name="last_name" value={newUser.last_name} onChange={handleInputChange}/>
+                                </div>
+                                <button className='btn btn-dark' type="submit">Login</button>
+                            </form>
+                            {
+                               errorSignUp && (
+
+                                    <div class="alert alert alert-danger alert-dismissible fade show" role="alert">
+                                        <strong>Wait!</strong> Username/Address/Phone/First and Last Name can't be blank
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={()=>setErrorSignUp(null)}></button>
+                                    </div>
+                                    
+                                )
+                            }
+                        </>
                     )}
                 </div>
             </div>
